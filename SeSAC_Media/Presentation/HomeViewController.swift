fdfb35524e3e78175dc6ae4c9dd3969c1b13b4a6
J.H. Moon <moonjh587@naver.com>
2023-08-11 17:43:45 +0900

    [Setting] Folding

---
+++ SeSAC_Media/Presentation/HomeViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  SeSAC_Media
+//
+//  Created by 문정호 on 2023/08/11.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
+
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        <#code#>
+    }
+    
+    
+}
